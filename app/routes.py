b4d8c6fc4a844f6703922de1cbea8a8b8a56186a
cccischk <cccischke05@gmail.com>
2025-10-29 12:44:15 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+from models import get_db_connection
 
 bp = Blueprint('main', __name__)
 
+
 @bp.route('/')
 def index():
+    db = get_db_connection()
+    cursor = db.cursor()
     return render_template('index.html', title='Home')
