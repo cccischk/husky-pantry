b19987d841b8ea46574cc42a96bc97ae60ffc51f
Sickmannnnnn <zack.sickmann@icloud.com>
2025-11-01 17:37:57 -0400

    flask setup fix

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,8 @@
-import sys, os
+
+
+import sys
+import os
+
 sys.path.insert(0, os.path.dirname(__file__))
 
-from app import create_app
-application = create_app()
+from run import app as application
